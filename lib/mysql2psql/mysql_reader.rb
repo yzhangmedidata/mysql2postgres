dfb91ef83a43bbe4886f102fd9ff0012fb7839b9
Yi Zhang <yzhang@mdsol.com>
2014-10-23 19:25:42 -0400

    commit

--- lib/mysql2psql/mysql_reader.rb
+++ lib/mysql2psql/mysql_reader.rb
@@ -168,7 +168,10 @@ class Mysql2psql
       @mysql = ::MysqlPR.connect(@host, @user, @passwd, @db, @port, @sock)
       @mysql.charset = ::MysqlPR::Charset.by_number 192 # utf8_unicode_ci :: http://rubydoc.info/gems/mysql-pr/MysqlPR/Charset
       @mysql.query('SET NAMES utf8')
-      @mysql.query('SET SESSION query_cache_type = OFF')
+      begin
+        @mysql.query('SET SESSION query_cache_type = OFF')
+      rescue MysqlPR::ServerError::QueryCacheDisabled
+      end
     end
 
     def reconnect
